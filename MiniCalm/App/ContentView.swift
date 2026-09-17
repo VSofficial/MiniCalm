d22e638383bfa73188ac835e254cce7fba057bcf
Varun <vsofficialmail@gmail.com>
2026-09-17 08:51:52 +0530

    Initial Architecture Setup

--- MiniCalm/App/ContentView.swift
+++ MiniCalm/App/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var items = [1,2,3,4]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            List() {
+                ForEach(items, id:\.self) { item in
+                    Text(String(item))
+                }
+            }
+            
         }
         .padding()
     }
