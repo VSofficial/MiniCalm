e6a4bc538de1e74849c335ad8300c68e325add1d
Varun <vsofficialmail@gmail.com>
2026-09-17 08:26:47 +0530

    Initial Commit

---
+++ MiniCalm/MiniCalmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniCalmApp.swift
+//  MiniCalm
+//
+//  Created by Varun Sharma on 17/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MiniCalmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
